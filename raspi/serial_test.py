e5b566a2b06d2481246235631bf27474bcd24bf0
marceldubach <marcel.dubach@epfl.ch>
2020-11-09 20:59:13 +0100

    add serial test

--- raspi/serial_test.py
+++ raspi/serial_test.py
@@ -14,9 +14,15 @@ if __name__=='__main__':
         if ser.in_waiting>0:
             line = ser.readline().decode('utf-8').rstrip()
             # rstrip removes the '\n' at the end of the line
-            print(line)
+
+            potential = float(line)
+            if (potential > 500):
+                ser.write(b"led\n")
+            else:
+                ser.write(b"led2\n")
 
             end_time = time.time()
+
             print("End time:" + str(end_time))
             if(end_time-start_time>10):
                 running = False
