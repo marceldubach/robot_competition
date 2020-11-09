362d69f0d65520b556dc537d24b58f5dcd125845
marceldubach <marcel.dubach@epfl.ch>
2020-11-09 17:55:06 +0100

    update image name

--- code/picam.py
+++ code/picam.py
@@ -1,4 +1,3 @@
-print("Hello world!")
 
 from picamera import PiCamera
 from datetime import datetime
@@ -6,10 +5,12 @@ from datetime import datetime
 camera = PiCamera()
 camera.rotation = 180
 
+print("Hello world!")
+
 now  = datetime.now()
-date_time = now.strftime("%Y/%m/%d-%S:%M:%S")
+date_time = now.strftime("%Y/%m/%d-%H/%M/%S")
 
 import time
 #camera.capture("image-"+date_time+".jpg")
 print(date_time+".jpg")
-camera.capture(date_time+"jpg")
+camera.capture("images/foo.jpg")
