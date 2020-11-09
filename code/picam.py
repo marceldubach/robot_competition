ecc5655a5ff267a9d3c60f40a4d60a2b50dbbc4b
marceldubach <marcel.dubach@epfl.ch>
2020-11-09 17:43:18 +0100

    save image to images folder

--- code/picam.py
+++ code/picam.py
@@ -7,7 +7,7 @@ camera = PiCamera()
 camera.rotation = 180
 
 now  = datetime.now()
-date_time = now.strftime("%Y/%m/%d,%S:%M:%S")
+date_time = now.strftime("%Y/%m/%d-%S:%M:%S")
 
 import time
 camera.capture("image-"+date_time+".jpg")
