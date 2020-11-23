3bf407c0f69c59ee1428ed05f9a77dd3f4206c5a
marceldubach <marcel.dubach@epfl.ch>
2020-11-23 08:45:01 +0100

    change picam.py to take arguments

--- raspi/picam.py
+++ raspi/picam.py
@@ -6,12 +6,12 @@ parser = argparse.ArgumentParser()
 
 args = parser.add_argument()
 parser.parseargs("name",help="name the photo to save as .jpg")
-parser.parseargs("-c","--cam",help="camera type: set to 1 for normal camera",type=int)
+parser.parseargs("-c","--cam",help="camera type: add -c to change to normal camera",action="store_true")
 camera = PiCamera()
 camera.rotation = 180
 
 cameraType = "noIR"
-if args.cam == 1:
+if args.cam:
     cameraType = "normal"
 
 camera.capture("images/"+cameraType+"/"+args.name+".jpg")
