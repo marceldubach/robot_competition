64f87ba289ca36510e2b3db8ab9675b78e2544b8
marceldubach <marcel.dubach@epfl.ch>
2020-12-02 16:27:50 +0100

    add some serial test

--- raspi/tests/JsonSerial/json_serial.py
+++ raspi/tests/JsonSerial/json_serial.py
@@ -6,15 +6,18 @@ ser = Serial('/dev/ttyAMA0', 9600, timeout=1)
 print("using port: ", ser.name)
 print("start serial port connection...")
 t_start = time.time()
+i = 0
 while True:
     print("send request")
     ser.write("poll".encode())
+    time.sleep(0.1)
     rcv = ser.readLine()
     print(rcv)
     time.sleep(1)
-
+    i += 1
+    print(i)
     t_current = time.time()
-    if (t_current-t_start)>10:
+    if (i>5):
         print("exciting")
         break
 
