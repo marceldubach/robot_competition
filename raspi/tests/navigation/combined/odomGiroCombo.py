be4839892b53a8b2afe6e90ce91b6a4f4eafc205
LorenzoPanchetti <lorenzo.panchetti@epfl.ch>
2020-12-07 14:10:39 +0100

    combo

---
+++ raspi/tests/navigation/combined/odomGiroCombo.py
@@ -0,0 +1,38 @@
+import serial
+import json
+import time
+from odometry import odometry
+from gyro import angleComputation
+import numpy as np
+
+# Assumption 50Hz signals 
+l = 0.144
+r = 0.04
+theta = 0
+pose = np.array([[1, 1, 0]]).transpose()
+print(pose,theta)
+time.sleep(3)
+
+if __name__ == '__main__':
+    
+    ser = serial.Serial('/dev/ttyACM0', 38400, timeout=1)
+    ser.flush()
+    time_start = time.time()
+    while True:
+        if ser.in_waiting > 0:
+            decoded = json.loads(ser.readline())
+            gz = decoded["gyro"]
+            if (abs(decoded["motorSpeed"][0]-415)< 10):
+                decoded["motorSpeed"][0] = 415
+            if (abs(decoded["motorSpeed"][1]-415)< 10):
+                decoded["motorSpeed"][1] = 415
+            avSpeedR = -((float(decoded["motorSpeed"][0])-415.00)/415.00)*6.25 #rad/s
+            avSpeedL = ((float(decoded["motorSpeed"][1])-415.00)/415.00)*6.25  #rad/s
+            omega = [avSpeedL, avSpeedR]
+            time_end = time.time()
+            t = time_end - time_start 
+            pose = odometry(l, r, pose, omega, t)
+            theta = angleComputation(theta, gz, t)
+            print(pose, theta)
+            time_start = time.time()
+            time.sleep(0.02)
