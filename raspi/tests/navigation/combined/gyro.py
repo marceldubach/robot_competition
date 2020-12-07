ecfd65c194d05a429554a34534fc3bb26825745e
LorenzoPanchetti <lorenzo.panchetti@epfl.ch>
2020-12-07 10:01:16 +0100

    small typo

--- raspi/tests/navigation/combined/gyro.py
+++ raspi/tests/navigation/combined/gyro.py
@@ -7,6 +7,6 @@ def angleComputation(psi, gz, dt):
     # psi: yaw angle of previous iteration [rad]
     # gz: angular velocity rate with respect to z [°/sec] 
     # Transform gz from [°/s] in [rad/s]
-    gz = 2*math.pi*gz/180 
+    gz = math.pi*gz/180 
     newPsi = psi + gz*dt
     return newPsi
