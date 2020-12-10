d5912c50948cb4a96046313fd1c9c7e0f5419851
PanchettiLorenzo <lorenzo.panchetti@epfl.ch>
2020-12-10 14:33:15 +0000

    odomGyro

--- raspi/tests/navigation/odometryUsingGyro/odomWithGyro.py
+++ raspi/tests/navigation/odometryUsingGyro/odomWithGyro.py
@@ -10,14 +10,14 @@ import numpy as np
 r = 0.04
 theta = 0
 pose = np.array([[1, 1, 0]]).transpose()
-t = 0.02
+#t = 0.02
 print(pose,theta)
 
 if __name__ == '__main__':
     
     ser = serial.Serial('/dev/ttyACM0', 38400, timeout=1)
     ser.flush()
-    #time_start = time.time()
+    time_start = time.time()
     while True:
         if ser.in_waiting > 0:
             decoded = json.loads(ser.readline())
@@ -29,12 +29,12 @@ if __name__ == '__main__':
             avSpeedR = -((float(decoded["motorSpeed"][0])-415.00)/415.00)*6.25 #rad/s
             avSpeedL = ((float(decoded["motorSpeed"][1])-415.00)/415.00)*6.25  #rad/s
             omega = [avSpeedL, avSpeedR]
-            #time_end = time.time()
-            #t = time_end - time_start 
+            time_end = time.time()
+            t = time_end - time_start 
             pose = odometryGyro(gz, r, pose, omega, t)
             theta = angleComputation(theta, gz, t)
             print(pose, theta)
-            #time_start = time.time()
+            time_start = time.time()
         else:
             #time_start = time.time()
             pass
