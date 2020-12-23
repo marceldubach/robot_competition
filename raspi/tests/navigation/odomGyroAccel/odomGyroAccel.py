fbcda0dca26a462d3ffc5c9b43f5abb5cfc25f5e
marceldubach <marcel.dubach@epfl.ch>
2020-12-23 16:44:19 +0100

    add try catch for serial reading

--- raspi/tests/navigation/odomGyroAccel/odomGyroAccel.py
+++ raspi/tests/navigation/odomGyroAccel/odomGyroAccel.py
@@ -21,26 +21,31 @@ if __name__ == '__main__':
     #time_start = time.time()
     while True:
         if ser.in_waiting > 0:
-            decoded = json.loads(ser.readline())
-            gz = decoded["gyro"]
-            ax = decoded["accel"]
-            t = float(decoded["dT"])/1000.0
-            if (abs(decoded["motorSpeed"][0]-415)< 10):
-                decoded["motorSpeed"][0] = 415
-            if (abs(decoded["motorSpeed"][1]-415)< 10):
-                decoded["motorSpeed"][1] = 415
-            avSpeedR = -((float(decoded["motorSpeed"][0])-415.00)/415.00)*6.25 #rad/s
-            avSpeedL = ((float(decoded["motorSpeed"][1])-415.00)/415.00)*6.25  #rad/s
-            omega = [avSpeedL, avSpeedR]
-            #time_end = time.time()
-            #t = time_end - time_start 
-            pose = odometryGyroAccel(gz, ax, r, pose, omega, t)
-            theta = angleComputation(theta, gz, t)
-            #jsonString = json.dumps(theta)
-            #ser.write(jsonString)
-            if pose[0] > 3:
-                ser.write(b"stop\n")
-            print(pose)
+            try:
+                decoded = json.loads(ser.readline())
+
+                gz = decoded["gyro"]
+                ax = decoded["accel"]
+                t = float(decoded["dT"])/1000.0
+                if (abs(decoded["motorSpeed"][0]-415)< 10):
+                    decoded["motorSpeed"][0] = 415
+                if (abs(decoded["motorSpeed"][1]-415)< 10):
+                    decoded["motorSpeed"][1] = 415
+                avSpeedR = -((float(decoded["motorSpeed"][0])-415.00)/415.00)*6.25 #rad/s
+                avSpeedL = ((float(decoded["motorSpeed"][1])-415.00)/415.00)*6.25  #rad/s
+                omega = [avSpeedL, avSpeedR]
+                #time_end = time.time()
+                #t = time_end - time_start
+                pose = odometryGyroAccel(gz, ax, r, pose, omega, t)
+                theta = angleComputation(theta, gz, t)
+                #jsonString = json.dumps(theta)
+                #ser.write(jsonString)
+                if pose[0] > 3:
+                    ser.write(b"stop\n")
+                print(pose)
+            except:
+                print("Didnt decode value")
+                pass
             #time_start = time.time()
         else:
             pass
