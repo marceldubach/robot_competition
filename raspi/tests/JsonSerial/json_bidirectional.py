2c274b07e45a0492e8792fbb987f0da0389e856b
marceldubach <marcel.dubach@epfl.ch>
2020-12-23 15:47:43 +0100

    print only from arduino

--- raspi/tests/JsonSerial/json_bidirectional.py
+++ raspi/tests/JsonSerial/json_bidirectional.py
@@ -12,28 +12,33 @@ from serial import Serial
 import numpy as np
 
 # set the same baudrate as in Arduino script!!!
-ser = Serial('/dev/ttyAMA0', 9600, timeout=1)
-print("[SER] selected port: ", ser.name)
-if (ser.isOpen()):
-    print("[SER] serial connection is open")
-else:
-    print("[SER] serial connection failed")
+if __name__=='__main__':
+    ser = Serial('/dev/ttyAMA0', 9600, timeout=1)
+    print("[SER] selected port: ", ser.name)
+    if (ser.isOpen()):
+        print("[SER] serial connection is open")
+    else:
+        print("[SER] serial connection failed")
 
-start_time = time.time()
-ser.flush()
-send_times = np.random.rand(10)+ np.arange(0,10)
-print("send times:", send_times)
-i = 0
-while(i<10):
-    # if ser.in_waiting>0:
-    #     decoded = json.loads(ser.readline())
-    #     print(decoded)
+    start_time = time.time()
+    ser.flush()
+    send_times = np.random.rand(10)+ np.arange(0,10)
+    print("send times:", send_times)
+    i = 0
+    while(i<10):
+        if (ser.in_waiting > 0):
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            i+=1
 
-    if (send_times[i]<time.time()):
 
-        ser.write(str(i*10).encode(encoding='UTF-8'))
-        print("send delay:", time.time()-send_times[i])
-        i+=1
+
+
+
+
+
+
+
 
 ser.close()
 
